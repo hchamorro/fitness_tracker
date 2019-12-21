f59059db0bf726a7730b91cf2fde76cac45416be
Hannah Chamorro <hannah.chamorro@gmail.com>
2019-12-21 13:54:42 -0600

    db schema

---
+++ models/personalinfo.js
@@ -0,0 +1,59 @@
+module.exports = (sequelize, DataTypes) => {
+  const PersonalInfo = sequelize.define("PersonalInfo", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    birthDate: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      len: [1, 2]
+    },
+    birthMonth: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      len: [1, 2]
+    },
+    birthYear: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      len: [4, 4]
+    },
+    height: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: false
+    },
+    venmo:{
+        type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+    }
+  });
+};
+  PersonalInfo.associate = (models) => {
+    // We're saying that a PersonalInfo should belong to an Author
+    // A PersonalInfo can't be created without an Author due to the foreign key constraint
+    PersonalInfo.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+ 
+
+  return PersonalInfo;
+};
---
+++ models/user.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    // Giving the User model a name of type STRING
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: 1 }
+    }
+  });
+
+  User.associate = models => {
+    // Associating User with check ins
+    // When an User is deleted, also delete any associated Posts
+    User.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
+
+  return User;
+};

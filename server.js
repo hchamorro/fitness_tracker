ec0b5930a7bb473051df0a5929c0e353c561cf7f
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-01-04 10:34:03 -0600

    server

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const db = require("./models");
+
+const postroutes = require("./controllers/routes/posts-api-routes");
 
 /* Express App setup */
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-const db = require('./models');
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Static directory
 app.use(express.static("./public"));
 
+// Routes
+require("./controllers/routes/html-routes")(app);
+app.use(postroutes);
 // Start server to begin listening
 
-//Change to db.sequelize.sync({ force: true }) to reset db for testing
+// app.listen(PORT, () => {
+//   console.log(`Server listening on: http://localhost:${PORT}`);
+// });
+
+//Eventually change to this once we add sequalize/SQL database
+
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log('App listening on PORT ' + PORT);
+    console.log("App listening on PORT " + PORT);
   });
 });

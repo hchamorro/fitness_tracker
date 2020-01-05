78c5f8503e849b393ee5cd97272c5b4560113d23
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-01-05 13:01:07 -0600

    db update user

--- seeders/20200104184125-PersonalInfos-data.js
+++ seeders/20200104184125-PersonalInfos-data.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -8,60 +8,56 @@ module.exports = {
     */
 
     return queryInterface.bulkInsert(
-      'PersonalInfos',
+      "PersonalInfos",
       [
         {
-          firstName: 'First1',
-          lastName: 'Last1',
-          email: 'email1@email.com',
-          birthDate: '1',
-          birthMonth: '1',
-          birthYear: '1111',
-          height: '11',
-          weight: '111',
-          venmo: '@givememoney1',
+          firstName: "First1",
+          lastName: "Last1",
+          birthDate: "1",
+          birthMonth: "1",
+          birthYear: "1111",
+          height: "11",
+          weight: "111",
+          venmo: "@givememoney1",
           createdAt: new Date(),
           updatedAt: new Date(),
           UserId: 1
         },
         {
-          firstName: 'First2',
-          lastName: 'Last2',
-          email: 'email2@email.com',
-          birthDate: '2',
-          birthMonth: '2',
-          birthYear: '2222',
-          height: '22',
-          weight: '222',
-          venmo: '@givememoney2',
+          firstName: "First2",
+          lastName: "Last2",
+          birthDate: "2",
+          birthMonth: "2",
+          birthYear: "2222",
+          height: "22",
+          weight: "222",
+          venmo: "@givememoney2",
           createdAt: new Date(),
           updatedAt: new Date(),
           UserId: 2
         },
         {
-          firstName: 'First3',
-          lastName: 'Last3',
-          email: 'email3@email.com',
-          birthDate: '3',
-          birthMonth: '3',
-          birthYear: '3333',
-          height: '33',
-          weight: '333',
-          venmo: '@givememoney3',
+          firstName: "First3",
+          lastName: "Last3",
+          birthDate: "3",
+          birthMonth: "3",
+          birthYear: "3333",
+          height: "33",
+          weight: "333",
+          venmo: "@givememoney3",
           createdAt: new Date(),
           updatedAt: new Date(),
           UserId: 3
         },
         {
-          firstName: 'First4',
-          lastName: 'Last4',
-          email: 'email4@email.com',
-          birthDate: '4',
-          birthMonth: '4',
-          birthYear: '4444',
-          height: '44',
-          weight: '444',
-          venmo: '@givememoney4',
+          firstName: "First4",
+          lastName: "Last4",
+          birthDate: "4",
+          birthMonth: "4",
+          birthYear: "4444",
+          height: "44",
+          weight: "444",
+          venmo: "@givememoney4",
           createdAt: new Date(),
           updatedAt: new Date(),
           UserId: 4
@@ -77,6 +73,6 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
     */
 
-    return queryInterface.bulkDelete('PersonalInfos', null, {});
+    return queryInterface.bulkDelete("PersonalInfos", null, {});
   }
 };

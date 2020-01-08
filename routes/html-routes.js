26aa4c2ef4d9093f9c107ff960c0d846552243eb
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-01-07 19:24:22 -0600

    home handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,7 +5,7 @@
 // Dependencies
 // =============================================================
 // const path = require("path");
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 // Routes
@@ -17,27 +17,27 @@ const router = express.Router();
 //   });
 // };
 
-router.get("/", function(req, res) {
-  res.render("index");
+router.get('/', function(req, res) {
+  res.render('index');
 });
 
-router.get("/getstarted", function(req, res) {
-  res.render("form");
+router.get('/getstarted', function(req, res) {
+  res.render('form');
 });
 
-router.get("/signup", function(req, res) {
-  res.render("signup");
+router.get('/signup', function(req, res) {
+  res.render('signup');
 });
 
-router.get("/tom1", function(req, res) {
-  res.render("tom1");
+router.get('/home', function(req, res) {
+  res.render('home');
 });
 
-router.get("/tom2", function(req, res) {
-  res.render("tom2");
+router.get('/tom2', function(req, res) {
+  res.render('tom2');
 });
 
-router.get("/tom3", function(req, res) {
-  res.render("tom3");
+router.get('/tom3', function(req, res) {
+  res.render('tom3');
 });
 module.exports = router;

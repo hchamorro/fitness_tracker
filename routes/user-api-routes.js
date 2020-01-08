18f5ff9fa767b9426cd2c64cade5b3cdac26cc4c
Andrew Chon <chonclan@gmail.com>
2020-01-07 19:04:42 -0600

    commented out userImage when creating user. (TEMP)

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,10 +1,10 @@
-const db = require("../models");
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const multer = require("multer");
+const db = require('../models');
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, "./uploads/");
+    cb(null, './uploads/');
   },
   filename: function(req, file, cb) {
     cb(null, file.originalname);
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     cb(null, true);
   } else {
     //file rejected
@@ -28,13 +28,13 @@ const upload = multer({
 });
 const router = express.Router();
 
-router.get("/api/user", (req, res) => {
+router.get('/api/user', (req, res) => {
   db.User.findAll({}).then(result => {
     res.json(result);
   });
 });
 //GET route for 1 user
-router.get("/api/user/:id", (req, res) => {
+router.get('/api/user/:id', (req, res) => {
   db.User.findOne({
     where: {
       id: req.params.id
@@ -44,7 +44,7 @@ router.get("/api/user/:id", (req, res) => {
   });
 });
 //POST route to add a new user
-router.post("/api/user", upload.single("userImage"), (req, res) => {
+router.post('/api/user', upload.single('userImage'), (req, res) => {
   console.log(req.file);
   db.User.findOne({
     where: {
@@ -53,7 +53,7 @@ router.post("/api/user", upload.single("userImage"), (req, res) => {
   }).then(user => {
     if (user) {
       return res.status(409).json({
-        message: "user name already exists"
+        message: 'user name already exists'
       });
     } else {
       bcrypt.hash(req.body.password, 8, (err, hash) => {
@@ -66,10 +66,10 @@ router.post("/api/user", upload.single("userImage"), (req, res) => {
           db.User.create({
             userName: req.body.userName,
             email: req.body.email,
-            password: hash,
-            userImage: req.file.path
+            password: hash
+            // userImage: req.file.path
           }).then(result => {
-            console.log("1 entry successfully added");
+            console.log('1 entry successfully added');
             res.json(result);
           });
         }
@@ -89,7 +89,7 @@ router.post("/api/user", upload.single("userImage"), (req, res) => {
 //   });
 // });
 //PUT route to edit user
-router.put("/api/user", (req, res) => {
+router.put('/api/user', (req, res) => {
   db.User.update(
     {
       userName: req.body.userName
@@ -100,7 +100,7 @@ router.put("/api/user", (req, res) => {
       }
     }
   ).then(result => {
-    console.log("1 entry edited successfully");
+    console.log('1 entry edited successfully');
     res.json(result);
   });
 });

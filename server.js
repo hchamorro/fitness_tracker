b50427f7cbe0f70d3b5146f3b5b4b32cf542f262
Andrew Chon <chonclan@gmail.com>
2020-01-11 11:15:35 -0600

    enabled database for images

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const db = require('./models');
+// const bodyParser = require('body-parser');
 
 const htmlRoute = require('./routes/html-routes');
 const userApiRoute = require('./routes/user-api-routes');
@@ -13,8 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '10mb', extended: true }));
 
 //handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));

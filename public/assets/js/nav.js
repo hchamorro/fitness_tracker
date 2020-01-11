acb8fc4811a96ca2c71a432fe3217fa04750f236
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-01-11 12:49:27 -0600

    button event change page and update info

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,27 +1,32 @@
-console.log("nav");
+console.log('nav');
 $(document).ready(function() {
   const hamburgerDisplayLink = () => {
-    document.getElementById("logo").hidden = !document.getElementById("logo")
+    document.getElementById('logo').hidden = !document.getElementById('logo')
       .hidden;
-    document.getElementById("mobile-links").hidden = !document.getElementById(
-      "mobile-links"
+    document.getElementById('mobile-links').hidden = !document.getElementById(
+      'mobile-links'
     ).hidden;
   };
 
   const signInIndex = () => {
-    document.getElementById("signin-showing").hidden = false;
+    document.getElementById('signin-showing').hidden = false;
   };
 
-  $("#go-home-click").click(function() {
+  $('#go-home-click').click(function() {
     location.assign(`/`);
   });
 
-
-  $("#nav-profile").click(function() {
-    location.assign(`/profile`);
+  $('#nav-profile').click(function() {
+    const url = window.location.search;
+    let userId;
+    if (url.indexOf('?user_id=') !== -1) {
+      userId = url.split('=')[1];
+    }
+    let nextPage = `/profile?user_id=${userId}`;
+    location.assign(nextPage);
   });
 
-  $("#login-trigger").click(function() {
-    $("#login-content").slideToggle();
+  $('#login-trigger').click(function() {
+    $('#login-content').slideToggle();
   });
 });
